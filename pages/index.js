b2405baa92d3b63f24b10f80e8fb5f1cbc403c26
harpreetkhalsagtbit <harpreet.singh@adpushup.com>
2023-03-06 16:24:23 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import tw, { styled } from 'twin.macro'
+
+const styles = {
+  // Move long class sets out of jsx to keep it scannable
+  container: ({ hasBackground }) => [
+    tw`flex flex-col items-center justify-center h-screen`,
+    hasBackground && tw`bg-red-100`,
+  ],
+}
+
+const StyledH1 = styled.h1([
+  tw`
+    text-lg text-green-400
+  `
+])
+
+const IndexPage = () => (
+  <div css={styles.container({ hasBackground: true })}>
+    <div tw="flex flex-col justify-center h-full gap-y-5 ">
+      <StyledH1>hey</StyledH1>
+    </div>
+  </div>
+)
+
+export default IndexPage
